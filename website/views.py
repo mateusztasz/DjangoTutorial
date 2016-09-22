cc8968c33a430395bdc90936f5cd474fc29e4dac
tandaradei <nowickadaria1@gmail.com>
2016-09-22 20:11:00 +0100

    added static file that does not belong to any app

--- website/views.py
+++ website/views.py
@@ -8,6 +8,9 @@ class MainPageView(generic.ListView):
     context_object_name = 'AppList'
 
     def get_queryset(self):
-        return [app for app in website.settings.INSTALLED_APPS if not "django" in app]
+        # return [app for app in website.settings.INSTALLED_APPS if not "django" in app]
+        # the same functionality, but more in "Python style" :
+        return [app for app in website.settings.INSTALLED_APPS if "django" not in app]
+
 
 
